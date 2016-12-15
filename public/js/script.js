20ea94047aee02bb1c4657255857acce3f4632b3
Felipe <astorgadm@gmail.com>
2016-12-15 17:00:03 -0300

    Update in index.html.ejs

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,16 @@ function message(){
  
  $.ajax(get_travels);
 }
+
+
+// function ....(){
+
+//   var get_travels = {
+//    host: 'localhost',
+//    port: '5000',
+//    path: '/message',
+//    method: 'GET',
+//  };
+ 
+//  $.ajax(get_travels);
+// }
